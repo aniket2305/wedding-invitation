22ab2a17cc1a1477958a274588bfa686b317532d
aniket2305 <saxenaaniket@yahoo.com>
2021-01-07 20:09:17 +0100

    added text

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php Header(“main.html”); ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Aniket weds Bhakti</title>
+        <meta name="viewport" content="width=device-width,height = device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
 
-<?php echo "<p>Copyright</p>" ?>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+        <script type="text/javascript" src="./js/script.js"></script>
+    </head>
+    <body>
+
+        <?php echo "<h1>Hello Aniket</h1>" ?>
+
+    </body>
+</html>
